1dffe590535589c93fad4e421189e2af65a6d6ce
ismartkorea <ismartkorea@hotmail.com>
2014-04-17 05:48:54 +0900

    first commit;

---
+++ JavaSample/src/com/java200/math/MathOperMain.java
@@ -0,0 +1,13 @@
+package com.java200.math;
+ public class MathOperMain 
+ {
+ 	public static void main(String[] args) 
+ 	{
+ 		System.out.println(MathOper.banOlimAtNums(123.456,3));
+ 		System.out.println(MathOper.burimAtNums(123.456,3));
+ 		System.out.println(MathOper.olimAtNums(123.451,3));
+ 		System.out.println(MathOper.sinh(1));
+ 		System.out.println(MathOper.cosh(1));
+ 		System.out.println(MathOper.cosh(1));
+ 	}
+ }
